fd628c48ceb961f8fc702b2c490a4d784eb87e6a
Konstantin Perikov <pericov.ko@gmail.com>
2021-07-31 15:06:14 +0100

    add disjunction search

--- search.py
+++ search.py
@@ -2,16 +2,28 @@
 from typing import Set, Dict
 
 
-def search(query: str, index: Dict[str, Set]) -> Set:
+def conjunction_search(query: str, index: Dict[str, Set]) -> Set:
+    docsets = docset_extraction(index, query)
+    resultset = docsets[0].intersection(*docsets[1:])
+    return resultset
+
+
+def docset_extraction(index, query):
     keywords = query.split(" ")
     docsets = []
     for keyword in keywords:
         docset = index.get(keyword, [])
         docsets.append(docset)
-    resultset = docsets[0].intersection(*docsets[1:])
+    return docsets
+
+
+def disjunction_search(query: str, index: Dict[str, Set]) -> Set:
+    docsets = docset_extraction(index, query)
+    resultset = docsets[0].union(*docsets[1:])
     return resultset
 
 
 def test_search():
-    assert len(search("a", {"a": {1, 2, 3}})) == 3
-    assert len(search("a b", {"a": {1, 2, 3}, "b": {2, 3}})) == 2
+    assert len(conjunction_search("a", {"a": {1, 2, 3}})) == 3
+    assert len(conjunction_search("a b", {"a": {1, 2, 3}, "b": {2, 3}})) == 2
+    assert len(disjunction_search("a b", {"a": {1, 2, 3}, "b": {4, 5}})) == 5
